6de6d1593f8aee42640406005a38264ce96ba5dd
luulee <luulee@github.com>
2026-01-28 15:51:11 +0200

    nothing

--- Module 3.3/src/BankAccount.java
+++ Module 3.3/src/BankAccount.java
@@ -1,6 +1,40 @@
 public class BankAccount {
     // Your variable declarations and code here
 
+    private int money;
+    private int accountNumber;
+
+    private static int totalAccounts = 0;
+    private static int nextAccountNumber = 0;
+
+    BankAccount() {
+        int money = 0;
+    }
+    BankAccount(int money) {
+        this.money = money;
+        this.accountNumber = nextAccountNumber++;
+        totalAccounts ++;
+    }
+    public void deposit(int i) {
+        money += i;
+    }
+
+    public void withdraw(int i) {
+        money -= i;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getBalance() {
+        return money;
+    }
+    int count = 0;
+
+    public int getTotalAccounts() {
+        return totalAccounts;
+    }
 
 
     public static void main(String[] args) {
