532fd55ecc9b8b5fea80c670f4ea8fbf4a80b438
luulee <luulee@github.com>
2026-02-25 13:56:11 +0200

    module 6.4 wip

---
+++ Module 6.4/src/main/java/org/notebook/Main.java
@@ -0,0 +1,11 @@
+package org.notebook;
+
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        NotebookView.launch(NotebookView.class);
+    }
+}
